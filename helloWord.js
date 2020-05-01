0ed00e2ff8c4d2ec1fe2b354130cebdab52a19d3
Patricia Vitor <patriciavitoor@gmail.com>
2020-05-01 11:16:21 -0300

    HelloWord
    Inicio de declaração de função e variavel

---
+++ helloWord.js
@@ -0,0 +1,14 @@
+// Comando console 
+//console.log("Hello Word.");
+
+// Declaracao de uma variavel
+var texto = "Hello Word."
+
+//Criando uma função
+function imprimirTexto(TextoParaImprimir){
+   console.log(TextoParaImprimir);
+
+}
+
+//Chamando a Função
+imprimirTexto(texto);
